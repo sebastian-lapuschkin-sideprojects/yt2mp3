406f0cdf035099f8696e24ab10e524437d3b82aa
Sebastian Lapuschkin <sebastian.lapuschkin@googlemail.com>
2019-08-25 14:00:13 +0200

    checkin basic proof of concept files

---
+++ basic_conversion.sh
@@ -0,0 +1,1 @@
+ffmpeg -i _-1-3Fp7-CHdE.mp3 -f segment -segment_time 300 -c copy "Dune - Der Wüstenplanet (Teil 2)/%03d.mp3"
---
+++ basic_download.sh
@@ -0,0 +1,1 @@
+youtube-dl --ignore-errors -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 --download-archive downloaded.txt https://www.youtube.com/watch?v=xQH1xxN8Lpk
